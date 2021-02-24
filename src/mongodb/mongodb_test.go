9880c63ae5644785523a41241b721aa980daf1a3
pranav-geolabs <pranav@geometrylabs.io>
2021-02-23 20:20:06 -0800

    Add TestAllBlocks

--- src/mongodb/mongodb_test.go
+++ src/mongodb/mongodb_test.go
@@ -49,6 +49,43 @@ func TestFindBlockByHash(t *testing.T) {
 	if block.Hash != "4d7b862e4f49d23ca445a5df4e44585c9822563f115e60d0f544372797192102" ||
 		block.Number != 10000019 {
 		t.Errorf("Failed to validate mongodb data")
+
+		// Fail
+		return
+	}
+
+	// Pass
+	return
+}
+
+func TestAllBlocks(t *testing.T) {
+	mongodb_url_env := os.Getenv("ICON_GRAPHQL_API_MONGODB_URL")
+	mongodb_user_env := os.Getenv("ICON_GRAPHQL_API_MONGODB_USER")
+	mongodb_pass_env := os.Getenv("ICON_GRAPHQL_API_MONGODB_PASS")
+
+	err := ConnectClient(mongodb_url_env, mongodb_user_env, mongodb_pass_env)
+	if err != nil {
+		t.Errorf("Failed to connect mongo client: %s", err.Error())
+
+		// Fail
+		return
+	}
+
+	// Find block
+	// Mock data inserted at mongodb/init/mock.js
+	blocks, err := DBConnection.AllBlocks()
+	if err != nil {
+		t.Errorf("Failed to query mongodb: %s", err.Error())
+
+		// Fail
+		return
+	}
+
+	if len(blocks) == 0 {
+		t.Errorf("Failed to retrieve any blocks")
+
+		// Fail
+		return
 	}
 
 	// Pass
